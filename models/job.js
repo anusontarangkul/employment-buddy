34241c6ff81f3169b020dd630f044b9d09e452f6
Lee Marvin <ryenem8@gmail.com>
2020-10-29 14:02:12 -0700

    finished models

--- models/job.js
+++ models/job.js
@@ -3,17 +3,16 @@ module.exports = function(sequelize, DataTypes) {
       title: {
         type: DataTypes.STRING,
         allowNull: false,
-    
       },
       company: {
         type: DataTypes.STRING,
         allowNull: false
       },
-      created_at: {
-        type: 'TIMESTAMP',
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-        allowNull: false
-      },
+     status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "applied"
+     },
     });
     Job.associate = function(models) {
         Job.belongsTo(models.User, {
@@ -21,11 +20,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false
           }
         });
-        Job.belongsTo(models.Status, {
-            foreignKey: {
-            allowNull: false
-            }
-        });
+    
 }
 return Job
 }
